5c8e94979e3286ec054af8805f35be0e2bf58de1
Nauman Khan <naumankhan642@gmail.com>
2022-07-20 00:20:44 +0500

    Added Class Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Greet from './components/Greet'
+import Welcome from './components/Welcom';
 function App() {
   return (
     <div className="App">
     <Greet/>
+    <Welcome/>
     </div>
   );
 }
---
+++ src/components/Welcom.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react";
+
+class Welcome extends Component{
+    render(){
+        return(<h2>Welcome</h2>)
+    }
+}
+export default Welcome
